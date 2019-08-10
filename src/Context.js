e320c2eba0d5148bfb9fb2558f07c5e9649f6155
Tschi0 <qweetr86@gmail.com>
2019-08-10 13:00:50 +0200

    final setup for featured rooms

--- src/Context.js
+++ src/Context.js
@@ -17,19 +17,21 @@ export default class RoomProvider extends Component {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
     this.setState({
-        rooms, featuredRooms, sortedRooms:rooms, loading:false
-    })
-    
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false
+    });
   }
 
   formatData(items) {
     let tempItems = items.map(item => {
       let id = item.sys.id;
       let images = item.fields.images.map(image => image.fields.file.url);
-      let room = {...item.fields,images}
+      let room = { ...item.fields, images };
       return room;
     });
-    return tempItems
+    return tempItems;
   }
 
   render() {
